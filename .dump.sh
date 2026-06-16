8475fc6cedb2950c66195d1e77cb9ca280e2473e
不知火 Shiranui <supersonic@livemail.tw>
2026-06-16 12:15:06 +0800

    refactor: add dump scripts for database backup and update permissions

---
+++ .dump.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# illusion - docker-composer.yml of mariadb + redis + phpmyadmin, for sample only.
+# (c) 2026 Shiranui (https://shiranui.xyz)
+
+# Exits on error occurred
+set -e
+
+# Execute the dumpfiles script inside the container
+docker exec illusion-data dumpfiles
